03cbece7837c37a49fe78f13f919b8d5f095479e
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-09 15:26:13 +0700

    Reactive & ref

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,20 +1,26 @@
 <script setup>
-// Logic code
-const message = "Welcome to Vue.js Project Basic!";
-const a = 100;
-const array = [1, 2, 3, 4, 5];
-const user = {
-  name: "Dinh Van Thuan",
-  age: 30,
-};
-console.log(message);
+import { reactive, ref } from "vue";
+// ref sử dụng 1 đối tượng và 1 thuộc tính
+// truy cập thuộc tính bằng cách .value
+const count = ref(0);
+count.value = count.value + 1;
+
+// reactive sử dụng 1 đối tượng, trong đó có nhiều thuộc tính
+// truy cập thuộc tính bằng cách .thuộc_tính
+const count1 = reactive({
+  title: "Hello World",
+  author: "Dinh Van Thuan",
+  age: 18,
+});
+
+count1.age += 1;
+console.log("count1", count1.title, count1.author, count1.age);
 </script>
 
 <template>
-  <div class="red">{{ message }} {{ user }}</div>
+  <div class="red">This is {{ count }}</div>
   <div class="blue">
-    {{ user.name }} <br />
-    {{ user.age }}
+    This is {{ count1.title }} {{ count1.author }} {{ count1.age }}
   </div>
 </template>
 
