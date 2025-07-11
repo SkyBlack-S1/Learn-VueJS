686fec1ba39c328643f8e0abedadab987da6cd29
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-11 21:59:14 +0700

    Conditional

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,24 +11,51 @@ const handleIncrease = (data) => {
   console.log(`Increase by ${data.label}`);
 };
 
+const categoryProduct = ref("food");
+
 const products = ref([
-  { id: 1, name: "Pizza", price: 100 },
-  { id: 2, name: "Cafe", price: 200 },
-  { id: 3, name: "Milk", price: 300 },
+  { id: 1, name: "Pizza", category: "food", price: 100 },
+  { id: 2, name: "Hamburger", category: "food", price: 200 },
+  { id: 3, name: "Milk", category: "drink", price: 300 },
+  { id: 4, name: "Tea", category: "drink", price: 400 },
+  { id: 5, name: "Juice", category: "drink", price: 500 },
 ]);
+
+const handChangeCategoryProduct = (value) => {
+  categoryProduct.value = value;
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome :countView="count" @handle-increase="handleIncrease" />
-  </main>
-  <div>
-    <div v-for="product in products">
-      <div>
-        <div>{{ product.name }} {{ product.price }}</div>
+    <!-- <TheWelcome :countView="count" @handle-increase="handleIncrease" /> -->
+    <button @click="handChangeCategoryProduct('food')">Hiển thị food</button>
+    <button @click="handChangeCategoryProduct('drink')">Hiển thị drink</button>
+    <div v-if="categoryProduct === 'food'">
+      Danh sach Food
+      <div v-for="product in products">
+        <div v-if="product.category == 'food'">
+          {{ product.name }} - {{ product.price }}
+        </div>
       </div>
     </div>
-  </div>
+    <div v-else-if="categoryProduct === 'drink'">
+      Danh sach Drink
+      <div v-for="product in products">
+        <div v-if="product.category == 'drink'">
+          {{ product.name }} - {{ product.price }}
+        </div>
+      </div>
+    </div>
+
+    <!-- <div>
+      <div v-for="product in products">
+        <div>
+          <div>{{ product.name }} {{ product.price }}</div>
+        </div>
+      </div>
+    </div> -->
+  </main>
 </template>
 
 <style scoped>
