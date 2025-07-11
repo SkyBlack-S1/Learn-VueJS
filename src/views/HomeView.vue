edb330fb6d5737ab22ff52840754b5d1f47a2d58
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-11 20:59:10 +0700

    List Rendering
    Array thì dùng Ref | Object n array thì dùng Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,10 +10,29 @@ const handleIncrease = (data) => {
   count.value += data.value;
   console.log(`Increase by ${data.label}`);
 };
+
+const products = ref([
+  { id: 1, name: "Pizza", price: 100 },
+  { id: 2, name: "Cafe", price: 200 },
+  { id: 3, name: "Milk", price: 300 },
+]);
 </script>
 
 <template>
   <main>
     <TheWelcome :countView="count" @handle-increase="handleIncrease" />
   </main>
+  <div>
+    <div v-for="product in products">
+      <div>
+        <div>{{ product.name }} {{ product.price }}</div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+div {
+  font-weight: 600;
+}
+</style>
