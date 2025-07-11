40e0032926ca820b387d393cbefd01ef5e14271c
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-11 11:23:11 +0700

    Sử dụng useRouter chuyển trang - useRoute lấy thông tin của route hiện tại

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,22 @@
+<script setup>
+import { useRouter, useRoute } from "vue-router";
+const router = useRouter(); // Lấy đối tượng điều hướng (router)
+const route = useRoute(); // 	Lấy thông tin của route hiện tại (params, query, path,...)
+console.log("path", route.path);
+console.log("name", route.name);
+console.log("params", route.params);
+console.log("query", route.query);
+</script>
 <template>
   <div class="about">
+    <!-- Cách 1 -->
+    <!-- <button @click="router.push({ name: 'user' })">Redirect About User</button> -->
+    <!-- <button @click="router.push({ path: '/about/user' })">
+      Redirect About User
+    </button> -->
+    <!-- Cách 2 -->
+    <RouterLink to="/about/user">Redirect About User</RouterLink>
     <router-view></router-view>
-    <RouterLink to="/about/user">Address</RouterLink>
   </div>
 </template>
 
