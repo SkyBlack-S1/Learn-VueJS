13f41a6958cb7e6d1a456759f39b2da2eb82119f
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-11 15:32:40 +0700

    Props & Function Props
    Cách truyền dữ liệu từ cha sang con và ngược lại

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,18 +1,15 @@
+<!-- Child -->
 <script setup>
-import { ref } from "vue";
-
-const textInput = ref("");
-console.log("Rerender Component", textInput.value);
-
-const name = ref("");
+const props = defineProps(["countView"]);
+const emit = defineEmits(["handle-increase"]);
+const onIncrease = () => {
+  emit("handle-increase", { label: "Hello", value: 5 }); // Truyền cho cha
+};
 </script>
 
 <template>
-  <div>Text: {{ textInput }}</div>
-  <input type="text" v-model="textInput" />
-  <br />
-  <input v-model.trim="name" placeholder="Nhập tên của bạn" />
-  <p>Xin chào {{ name }}!</p>
+  <div>Count: {{ props.countView }}</div>
+  <button @click="onIncrease">Increase</button>
 </template>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
+<!-- Parent -->
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+import TheWelcome from "../components/TheWelcome.vue";
+
+const count = ref(10);
+
+const handleIncrease = (data) => {
+  // data là object { label: "Hello", value: 5 }
+  count.value += data.value;
+  console.log(`Increase by ${data.label}`);
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheWelcome :countView="count" @handle-increase="handleIncrease" />
   </main>
 </template>
